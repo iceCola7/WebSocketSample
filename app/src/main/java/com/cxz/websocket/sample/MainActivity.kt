b8e3b0672bcf64754508f4901678b0de3f3b5b27
chenxz <563859095@qq.com>
2021-04-08 09:39:10 +0800

    commit

--- app/src/main/java/com/cxz/websocket/sample/MainActivity.kt
+++ app/src/main/java/com/cxz/websocket/sample/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.cxz.websocket.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cxz.websocket.WebSocketCallback
+import com.cxz.websocket.WebSocketClient
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WebSocketCallback {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        WebSocketClient.instance.setWebSocketCallback(this)
     }
+
+    override fun onMessage(data: String) {
+        super.onMessage(data)
+    }
+
 }
